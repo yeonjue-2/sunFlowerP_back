b6b822a958dba6c60e28958d416eb83452dae51d
yeonjue-2 <ilov-@naver.com>
2023-01-15 21:17:42 +0900

    docs: yml로 수정 및 mysql 추가

---
+++ src/test/java/io/sunflower/SunflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sunflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
